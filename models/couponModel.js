bb037f4e091722b84b0f180c727b8a96b4d55ead
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2024-10-21 01:33:23 +0600

    fix: change blogcategory model coupon

--- models/couponModel.js
+++ models/couponModel.js
@@ -1,19 +1,8 @@
 const mongoose = require("mongoose");
 
 const couponSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        uppercase: true
-    },
-    expire: {
-        type: Date,
-        required: true,
-    },
-    discount: {
-        type: Number,
-        required: true,
-    }
+    name: { type: String, required: true },
+    discount: { type: Number, required: true },
+    expiry: { type: Date, required: true }, // This should be required
 });
 module.exports = mongoose.model("Coupon", couponSchema)
